140d28f8d47c6b53d670b08b994f1fdda3397eac
Jacky <gomjellie@gmail.com>
2021-02-04 15:34:56 +0900

    queue tests 추가

---
+++ queue_test2.c
@@ -0,0 +1,39 @@
+#include "queue.h"
+#include <assert.h>
+
+int main() {
+    queue *q = queue_alloc(1);
+    int *res;
+
+    queue_enqueue(q, 10);
+    queue_enqueue(q, 20);
+    queue_enqueue(q, 30);
+    queue_enqueue(q, 40);
+    queue_enqueue(q, 50);
+    queue_enqueue(q, 60);
+
+    res = queue_front(q);
+    assert(*res == 10);
+
+    queue_dequeue(q);
+    res = queue_front(q);
+    assert(*res == 20);
+
+    queue_dequeue(q);
+    res = queue_front(q);
+    assert(*res == 30);
+
+    queue_dequeue(q);
+    res = queue_front(q);
+    assert(*res == 40);
+
+    queue_dequeue(q);
+    res = queue_front(q);
+    assert(*res == 50);
+
+    queue_dequeue(q);
+    res = queue_front(q);
+    assert(*res == 60);
+
+    puts("queue expand test pass");
+}
