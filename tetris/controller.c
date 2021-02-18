898db0ff12c77e683fff04c3d8a4c3855c347b06
gomjellie <gomjellie@naver.com>
2021-02-18 18:16:29 +0900

    ncurses 라이브러리 함수들로 프린트 대체함

--- tetris/controller.c
+++ tetris/controller.c
@@ -1,9 +1,8 @@
-#include "keyboard.h"
 #include "view.h"
 #include "model.h"
 #include <stdio.h>
-
-
+#include <ncurses.h>
+#include <stdbool.h>
 
 int main() {
     int key;
@@ -12,33 +11,49 @@ int main() {
     int block[BLOCK_LEN][BLOCK_LEN];
     int next_block[BLOCK_LEN][BLOCK_LEN];
     int hold_block[BLOCK_LEN][BLOCK_LEN];
+    bool new_block_needed = false;
 
+    initscr();
+    keypad(stdscr, true);
+    noecho();
+    timeout(1000);
+    curs_set(false);
+    
     while (1) {
         tetris_get_board(model, board);
         tetris_get_block(model, block);
         tetris_get_next_block(model, next_block);
         tetris_get_hold_block(model, hold_block);
         
-        key = get_key();
+        key = getch();
+        // printf("%d\n", key);
         switch (key)
         {
         case KEY_UP:
+            render_msg("up  ");
             break;
         case KEY_DOWN:
+            render_msg("down ");
             break;
         case KEY_LEFT:
+            render_msg("left ");
             break;
         case KEY_RIGHT:
+            render_msg("right");
             break;
         default:
             break;
         }
+        refresh();
         tetris_update_board(model, board);
         render_board(model->board);
         render_next_block(model->block);
 
-        tetris_needs_new_block(model);
+        if (new_block_needed)
+            tetris_needs_new_block(model);
     }
+
+    endwin();
 
     tetris_free(model);
 }
